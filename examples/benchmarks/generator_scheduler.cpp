dd2baaa9b367de73426e0e4c7be16e779a76a4e7
Philippe Tillet <Phil.Tillet@gmail.com>
2013-07-06 20:52:27 +0200

    Added missing file

---
+++ examples/benchmarks/generator_scheduler.cpp
@@ -0,0 +1,113 @@
+/* =========================================================================
+   Copyright (c) 2010-2013, Institute for Microelectronics,
+                            Institute for Analysis and Scientific Computing,
+                            TU Wien.
+   Portions of this software are copyright by UChicago Argonne, LLC.
+
+                            -----------------
+                  ViennaCL - The Vienna Computing Library
+                            -----------------
+
+   Project Head:    Karl Rupp                   rupp@iue.tuwien.ac.at
+
+   (A list of authors and contributors can be found in the PDF manual)
+
+   License:         MIT (X11), see file LICENSE in the base directory
+============================================================================= */
+
+/*
+*
+*   Benchmark:   Vector operations (vector.cpp and vector.cu are identical, the latter being required for compilation using CUDA nvcc)
+*
+*/
+
+
+//#define VIENNACL_DEBUG_ALL
+#ifndef NDEBUG
+ #define NDEBUG
+#endif
+
+#include "viennacl/scalar.hpp"
+#include "viennacl/vector.hpp"
+#include "viennacl/linalg/inner_prod.hpp"
+#include "viennacl/linalg/norm_2.hpp"
+#include "viennacl/scheduler/execute.hpp"
+#include "viennacl/generator/generate.hpp"
+
+#include <iostream>
+#include <vector>
+#include "benchmark-utils.hpp"
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+#define BENCHMARK_VECTOR_SIZE   2
+#define BENCHMARK_RUNS          1000
+
+
+template<typename ScalarType>
+int run_benchmark()
+{
+
+  Timer timer;
+  double exec_time;
+
+  ScalarType std_result = 0;
+
+  std::vector<ScalarType> std_vec1(BENCHMARK_VECTOR_SIZE);
+  std::vector<ScalarType> std_vec2(BENCHMARK_VECTOR_SIZE);
+  std::vector<ScalarType> std_vec3(BENCHMARK_VECTOR_SIZE);
+  viennacl::vector<ScalarType> vcl_vec1(BENCHMARK_VECTOR_SIZE);
+  viennacl::vector<ScalarType> vcl_vec2(BENCHMARK_VECTOR_SIZE);
+  viennacl::vector<ScalarType> vcl_vec3(BENCHMARK_VECTOR_SIZE);
+
+
+  ///////////// Vector operations /////////////////
+
+  std_vec1[0] = 1.0;
+  std_vec2[0] = 1.0;
+  for (int i=1; i<BENCHMARK_VECTOR_SIZE; ++i)
+  {
+    std_vec1[i] = std_vec1[i-1] * ScalarType(1.000001);
+    std_vec2[i] = std_vec1[i-1] * ScalarType(0.999999);
+  }
+
+  viennacl::copy(std_vec1, vcl_vec1);
+  viennacl::fast_copy(std_vec1, vcl_vec1);
+  viennacl::copy(std_vec2, vcl_vec2);
+
+  viennacl::backend::finish();
+
+  std::vector<viennacl::scheduler::statement> my_statements;
+  my_statements.push_back(viennacl::scheduler::statement(vcl_vec2, viennacl::op_assign(), vcl_vec1 + vcl_vec2));
+  std::cout << viennacl::generator::make_program_name(my_statements) << std::endl;
+
+  return 0;
+}
+
+int main()
+{
+  std::cout << std::endl;
+  std::cout << "----------------------------------------------" << std::endl;
+  std::cout << "               Device Info" << std::endl;
+  std::cout << "----------------------------------------------" << std::endl;
+
+#ifdef VIENNACL_WITH_OPENCL
+  std::cout << viennacl::ocl::current_device().info() << std::endl;
+#endif
+
+  std::cout << std::endl;
+  std::cout << "----------------------------------------------" << std::endl;
+  std::cout << "----------------------------------------------" << std::endl;
+  std::cout << "## Benchmark :: Vector" << std::endl;
+  std::cout << "----------------------------------------------" << std::endl;
+  std::cout << std::endl;
+  std::cout << "   -------------------------------" << std::endl;
+  std::cout << "   # benchmarking single-precision" << std::endl;
+  std::cout << "   -------------------------------" << std::endl;
+  run_benchmark<float>();
+  return 0;
+}
+
